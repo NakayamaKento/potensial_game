8664fb310917a3be7466708cbc83c7c8faad6b1c
Unknown <nakayama@hlab.sys.es.osaka-u.ac.jp>
2019-02-06 17:14:31 +0900

    Auto stash before merge of "master" and "origin/master"
    Σホール作業分

--- potensial_game/potensial_game.py
+++ potensial_game/potensial_game.py
@@ -8,7 +8,7 @@ class agent:
 
 	def __init__(self):
 		self.Pass = 1
-        agent.pass1 += 1
+		agent.pass1 += 1
 
 	def change_pass(self, newpass):
 		if self.Pass == 1:
@@ -33,3 +33,35 @@ class agent:
 
 	def calc_Ce5(self, num):
 		return 6+num
+
+	def calc_Ui(self):
+		if self.Pass == 1:
+			return -( calc_Ce1(agent.pass1) + calc_Ce3(agent.pass1) )
+		elif self.Pass == 2:
+			return -( calc_Ce2(agent.pass2) + calc_Ce4(agent.pass2) )
+		elif self.Pass == 3:
+			return -( calc_Ce1(agent.pass3) + calc_Ce4(agent.pass3) + calc_Ce5(agent.pass3) )
+
+
+	def potential(self):
+		value
+		#e1の計算
+		for k in range(1, agent.pass1 + agent.pass3 + 1):
+			value += calc_Ce1(k)
+		#e2の計算
+		for k in range(1, agent.pass2 + 1):
+			value += calc_Ce2(k)
+		#e3の計算
+		for k in range(1, agent.pass1 + 1):
+			value += calc_Ce3(k)
+		#e4の計算
+		for k in range(1, agent.pass2 + agent.pass3 +1):
+			value += calc_Ce4(k)
+		#e5の計算
+		for k in range(1, agent.pass3 + 1):
+			value += calc_Ce5(k)
+
+#エージェントの初期化
+agent_list =['a', 'b', 'c', 'd']
+for N in agent_list:
+	agent_list = agent()
